4db98d283597a33df0bfc747404bdd6f603028f9
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-01 14:25:25 +0000

    Feb 1, 2024, 2:25 PM

---
+++ src/bike/Outer.java
@@ -0,0 +1,19 @@
+package bike;
+
+public class Outer {
+    public void outerMethod() {
+        System.out.println("outerMethod");
+    }
+
+    static class Nested_Demo
+    {
+        public void my_method() {
+            System.out.println("nested class");
+        }
+    }
+
+    public static void main(String[] args) {
+        Outer.Nested_Demo nested = new Outer.Nested_Demo();
+        nested.my_method();
+    }
+}
