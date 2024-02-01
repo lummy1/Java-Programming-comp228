d41506ea532b12d7579b32af0e49fdfa4f9ea586
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-01 14:04:38 +0000

    Feb 1, 2024, 2:04 PM

--- src/bike/Bike.java
+++ src/bike/Bike.java
@@ -1,5 +1,19 @@
 package bike;
 
 public class Bike {
+    static final int speedLimit = 90; // Assign a value to speedLimit
 
+    public void run() {
+        // speedLimit = 400; // This will cause an error because speedLimit is final
+        // Instead, you might want to create a new variable or make speedLimit non-final
+        int currentSpeed = 400;
+    }
+
+    public static void main(String[] args) {
+        Bike obj1 = new Bike();
+        obj1.run();
+
+        Bike obj2 = new Bike();
+        obj2.run(); // Added missing semicolon
+    }
 }
