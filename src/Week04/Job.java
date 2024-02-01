428c5498f88c3f4c7ae9259dd36314f29df8028d
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-01 14:34:00 +0000

    Feb 1, 2024, 2:34 PM

---
+++ src/Week04/Job.java
@@ -0,0 +1,41 @@
+package Week04;
+
+public class Job {
+    private String role;
+    private long salary;
+    private int id;
+
+    
+
+    public Job() {
+        
+    }
+
+
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public long getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
---
+++ src/Week04/Person.java
@@ -0,0 +1,14 @@
+package Week04;
+
+public class Person {
+    private Job job;
+
+    public Person() {
+        this.job = new Job();
+        job.setSalary(1000L);
+    }
+
+    public long getSalary() {
+        return job.getSalary();
+    }
+}
