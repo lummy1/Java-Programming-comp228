e35c6c3c7f4aa2fcaa62940dd14efa66851067b7
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-08 14:14:42 +0000

    Thursday, February 8, 2024 at 2:14 PM Coordinated Universal Time

---
+++ src/Week05/Person.java
@@ -0,0 +1,32 @@
+// Person class, the parent class
+package Week05;
+
+// PersonTest class, the driver class
+public class Person {
+    public static void main(String[] args) {
+        Person person1 = new Person("John Bailey");
+        System.out.println("Name of this person: " + person1);
+
+        Professor prof1 = new Professor("Syed Yamin", "SETAS", 001);
+        System.out.println(prof1);
+    }
+
+    protected String name; // instance variable type string
+
+    public Person(String aName) {
+        name = aName;
+    }
+
+    public void setName(String theNewName) {
+        name = theNewName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return ("\nName: " + name);
+    }
+}
---
+++ src/Week05/Professor.java
@@ -0,0 +1,27 @@
+package Week05;
+
+// Professor class, the inherited class
+
+public class Professor extends Person {
+    protected String department;
+    protected int id;
+
+    public Professor(String aName, String aDepartment, int anId) {
+        super(aName); // a call to the parent constructor, must be the first statement
+        department = aDepartment;
+        id = anId;
+    }
+
+    public void setDepartment(String aDepartment) {
+        department = aDepartment;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return ("\nName: " + name + "\nDepartment: " + department + "\nId: " + id);
+    }
+}
