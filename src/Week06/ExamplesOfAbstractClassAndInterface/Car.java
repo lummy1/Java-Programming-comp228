40bdb7903c157c4d5d7c84c6476899e49828a526
Dat Tran <datttrian@gmail.com>
2024-02-15 14:21:34 +0000

    Feb 15, 2024, 2:21 PM

---
+++ src/Week06/ExamplesOfAbstractClassAndInterface/Car.java
@@ -0,0 +1,39 @@
+package Week06.ExamplesOfAbstractClassAndInterface;
+
+public abstract class Car {
+    private String model;
+    private String color;
+    private int maxSpeed;
+
+    public abstract void accelerate();
+    public abstract void brake();
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return ("\nModel: " + model + "\nColor: " + color + "\nMax Speed: " + maxSpeed);
+    }
+}
