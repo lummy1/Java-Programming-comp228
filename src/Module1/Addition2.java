c858dec840fba594e27f4d7e25216ecda1462812
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-12 09:31:43 +0000

    Mar 12, 2024, 9:31 AM

--- src/Module1/Addition2.java
+++ src/Module1/Addition2.java
@@ -1,5 +1,24 @@
 package Module1;
 
-public class Addition2 {
+import javax.swing.JOptionPane;
+
+public class Addition2
+
+{
+    public static void main(String[] args)
+
+    {
+
+        int num1, num2, result;
+
+        num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter an integer: "));
+
+        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter another integer : "));
+
+        result = num1 + num2;
+
+        JOptionPane.showMessageDialog(null, "The result is " + result + ".");
+
+    }
 
 }
