e438cfff16b8218f651f0327e6456cd9c3364fe3
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-28 07:30:53 -0700

    Delete src/Module10/test1 directory

--- src/Module10/test1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.test1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.test1 to javafx.fxml;
-    exports org.example.test1;
-}
